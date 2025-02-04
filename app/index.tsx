346bbe24ce2eb9722fbf77464fd481893a653831
Your Name <you@example.com>
2025-02-04 13:48:49 +0200

    delete expo router files

--- app/index.tsx
+++
@@ -1,7 +0,0 @@
-// // src/app/index.tsx
-// import React from "react";
-// import RootStack from "../src/navigators/RootStack";
-
-// export default function App() {
-//   return <RootStack />; // Render the BottomBarTabs as the main navigator
-// }
